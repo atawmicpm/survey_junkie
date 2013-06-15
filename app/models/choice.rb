f308544b2cfffe92ea71e64754ce62cd4bd7a945
Lloyd Taylor <ltaylor@netelder.com>
2013-06-15 14:16:57 -0700

    added presence validations to :name in choice and question

--- app/models/choice.rb
+++ app/models/choice.rb
@@ -2,5 +2,7 @@ class Choice < ActiveRecord::Base
   belongs_to :question
   has_many :answers
   has_many :users, :through => :answers
+
+	validates_presence_of   :name	
   # Remember to create a migration!
 end
--- app/models/question.rb
+++ app/models/question.rb
@@ -1,5 +1,7 @@
 class Question < ActiveRecord::Base
   belongs_to :survey
   has_many :choices
+  
+  validates_presence_of   :name
   # Remember to create a migration!
 end
