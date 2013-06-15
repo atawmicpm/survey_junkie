7847ddc09440f4b33508ac6b6e060bfae7a127dd
Apprentice <apprentice@dbc04.local>
2013-06-14 20:00:14 -0700

    add questions and choices works

--- public/js/application.js
+++ public/js/application.js
@@ -1,4 +1,5 @@
 $(document).ready(function () {
+  var questionTemplate = $.trim($('#question-template').html());
 
   // send an HTTP DELETE request for the sign-out link
   $('a#sign-out').on("click", function (e) {
@@ -7,4 +8,33 @@ $(document).ready(function () {
     request.done(function () { window.location = "/"; });
   });
 
+  // add a new question to a survey
+  $('#add-question').on('click', function(e) {
+    e.preventDefault();
+    $('#all-questions').append(buildQuestion());
+  });
+
+  $('#all-questions').on('click', '.add-choice', function(e) {
+    e.preventDefault();
+    var question = $(this).prev();
+    question.append(buildChoice(question));
+  });
+
+  function buildQuestion() {
+    var totalQuestions = $('.survey-question').length;
+    var $question = $(questionTemplate);
+    $question.find('input').attr('name', function(i, val) {
+      return val.replace('q1', 'q' + totalQuestions);
+    });
+    return $question;
+  }
+
+  function buildChoice(question) {
+    var totalChoices = question.find('input').length;
+    var lastChoice = question.children().last();
+    var newChoice = lastChoice.clone().attr('name', function(i, val) {
+      return val.replace(/c\d/, 'c' + totalChoices);
+    });
+    return newChoice[0];
+  }
 });
